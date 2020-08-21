417f5d4f43aa2e2be187acb48e212ea982bb0a1d
Belinda Mustoe <belinda@verytwisty.com>
2020-08-21 16:15:29 +0100

    Expire posts after closing date

---
+++ admin/inc/expire-posts.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NHS_JOBS\ADMIN\ExpirePosts;
+
+
+if (! \wp_next_scheduled( 'expire_posts' )){
+
+  \wp_schedule_event( time(), 'hourly', 'nhsjobs_expire_posts' );
+
+}
+
+add_action('nhsjobs_expire_posts', __NAMESPACE__ . '\expire_opportunities');
+
+function expire_opportunities() {
+
+	$time = date('Y-m-d\TH:i:s');
+
+	$meta_quer_args = array(
+	'relation'  =>   'AND',
+	    array(
+	        'key'       =>   'nhsjobs_end',
+	        'value'     =>   $time,
+	        'compare'   =>   '<'
+	    )
+	);
+
+	$args = array(
+		'post_type'    =>   array( 'nhs_opportunities' ),
+		'nopaging'     =>   true,
+		'post_status'  =>   'publish',
+		'fields'       =>   'ids',
+		'meta_key'     =>   'nhsjobs_end',
+	    'orderby'      =>   'meta_value',
+	    'meta_query'   =>   $meta_quer_args,
+	);
+
+
+	$expired_posts = get_posts($args);
+
+	foreach ( $expired_posts as $post_id ):
+
+		$postdata = array(
+			'ID' => $post_id,
+			'post_status' => 'draft',
+			'post_date'   => '0000-00-00 00:00:00',
+			'post_date_gmt'   => '0000-00-00 00:00:00'
+		);
+
+		wp_update_post( $postdata );
+
+		update_post_meta( $post_id, 'nhsjobs_end', '' );
+
+	endforeach;
+}
+
