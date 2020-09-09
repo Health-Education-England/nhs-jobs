63e438664c808b0d368a1f75d5f571af0e297165
Belinda Mustoe <belinda@verytwisty.com>
2020-09-09 15:41:13 +0100

    Change cron job to twice dailey

--- admin/inc/expire-posts.php
+++ admin/inc/expire-posts.php
@@ -5,7 +5,7 @@ namespace NHS_JOBS\ADMIN\ExpirePosts;
 
 if (! wp_next_scheduled( 'expire_opportunities' )){
 
-  wp_schedule_event( time(), 'daily', 'nhsjobs_expire_posts' );
+  wp_schedule_event( time(), 'twicedaily', 'nhsjobs_expire_posts' );
 
 }
 
